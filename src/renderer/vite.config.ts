b261ad68dc489ef2936c62c90043713f7a92c66a
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2023-02-03 22:51:42 +0100

    dev: setup for local development

--- src/renderer/vite.config.ts
+++ src/renderer/vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte({ configFile: './renderer/svelte.config.js' })],
-  root: '../',
-  publicDir: '../public',
-  base: '',
-  build: {
-    outDir: '../build/',
-    assetsDir: './assets/',
-    emptyOutDir: true,
-  },
+export default defineConfig(({ mode }) => {
+  const processEnvValues = { ...process.env, ...loadEnv(mode, '../../') };
+
+  return {
+    plugins: [svelte({ configFile: './renderer/svelte.config.js' })],
+    root: '../',
+    publicDir: '../public',
+    base: '',
+    build: {
+      outDir: '../build/',
+      assetsDir: './assets/',
+      emptyOutDir: true,
+    },
+    server: {
+      port: parseInt(processEnvValues.VITE_PORT),
+    },
+  };
 });
